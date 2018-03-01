0d4276daddad5dcaa4ebcb0ee883dffc6202656d
Douglas McCandless <mccandlessd@gmail.com>
2018-03-01 12:40:41 -0500

    build out js for initGame(), examineLetters(), cycleComplete(); add placeholder image for .gameExtras aside

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,84 @@
 //global variables, objects, calls
 
 //document onload ready (?) needed for making sure the page is loaded before processing begins?
-//Global Variables
+
+
+//GLOBAL VARIABLES  ********************************************************************
+
 var stateCapitals = ["montgomery", "tallahassee", "annapolis", "bismarck", "columbia"];
 
-var computerSelection = stateCapitals[Math.floor(Math.random() * stateCapitals.length)];
-
-var montgomery = {
-  "state": "Alabama",
-  "outline": xxx.png,
-  "song": xxx.mp4
-}
-
-var tallahassee = {
-  "state": "Florida",
-  "outline": xxx.png,
-  "song": xxx.mp4
-}
-
-var annapolis = {
-  "state": "Maryland",
-  "outline": xxx.png,
-  "song": xxx.mp4
-}
-
-var bismarck = {
-  "state": "North Dakota",
-  "outline": xxx.png,
-  "song": xxx.mp4
-}
-
-var columbia = {
-  "state": "South Carolina",
-  "outline": xxx.png,
-  "song": xxx.mp4
-}
+var computerSelection = "";
 
 var winCounter = 0;
 
-var guessesRemaining = x; //how to initialize? initialize with 15
+var guessesRemaining = 10;
+
+var numDashes = 0;
+
+var lettersInSelection = [];
 
 var wrongLettersUsed = [];
 
-var currentContest[];
+var currentContest = [];
 
-var gameStarted = false;
+//var gameStarted = false;
+
+//for gameExtras functionality, each stateCapitals[i] is an object with properties to display
+
+var montgomery = {
+  "state": "Alabama"
+  /*"outline": xxx.png,
+  "flag": xxx.png*/
+}
+
+var tallahassee = {
+  "state": "Florida"
+  /*"outline": xxx.png,
+  "flag": xxx.png*/
+}
+
+var annapolis = {
+  "state": "Maryland"
+  /*"outline": xxx.png,
+  "flag": xxx.png*/
+}
+
+var bismarck = {
+  "state": "North Dakota"
+  /*"outline": xxx.png,
+  "flag": xxx.png*/
+}
+
+var columbia = {
+  "state": "South Carolina"
+  /*"outline": xxx.png,
+  "flag": xxx.png*/
+}
 
 
-//Functions
+
+
+//FUNCTIONS ***********************************************************************
 
 //for starting a game
-document.onkeyup = function(event){
+/*document.onkeyup = function(event){
   gameStarted = true;
   initGame();
-
-}
+}*/
 
 
 var initGame = function(){
 
   guessesRemaining = 10;
   currentContest = [];
+  wrongLettersUsed = [];
   
 
   computerSelection = stateCapitals[Math.floor(Math.random() * stateCapitals.length)];
   var lettersInSelection = computerSelection.split(" ");
   var numDashes = lettersInSelection.length;
 
-  for(var i = 0; i < numDashes; numDashes++){
+  for(var i = 0; i < numDashes; i++){
     currentContest.push(" _ ");
   }
 
@@ -84,20 +95,92 @@ var initGame = function(){
 
 }//end of initGame()
 
+//for checking letters
+
+var examineLetters = function(letter){
+  var isLetterInWord = false;
+  for (var i = 0; i < numDashes; i++){
+    if(computerSelection[i] === letter){
+      isLetterInWord = true;
+    }
+  }
+
+  if(isLetterInWord) {
+    for(var i = 0; i < numDashes; i++){
+      if(computerSelection[i] === letter){
+        currentContest[i] === letter;
+      }
+    }
+  }
+
+  else{
+    wrongLettersUsed.push(letter);
+    guessesRemaining--;
+  }
+
+  console.log(currentContest);
+} //end of examineLetters()
+
+var cycleComplete = function(){
+  console.log("Win count: " + winCounter + " | Guesses Left: " + guessesRemaining);
+
+  var html =
+        "<p>Press any key to get started</p>" +
+        "<p>Wins: " + winCounter + "</p>" +
+        "<p>Current Word: " + currentContest + "</p>" +                                    
+        "<p>Number of guesses remaining: " + guessesRemaining + "</p>" +
+        "<p>Letters already used: " + wrongLettersUsed + "</p>"; 
+        
+
+  document.querySelector("#gameDashboard").innerHTML = html;
+
+  if(lettersInSelection.toString() === currentContest.toString()){
+    winCounter++;
+    alert("You won!");
+
+    var html =
+        "<p>Press any key to get started</p>" +
+        "<p>Wins: " + winCounter + "</p>" +
+        "<p>Current Word: " + currentContest + "</p>" +                                    
+        "<p>Number of guesses remaining: " + guessesRemaining + "</p>" +
+        "<p>Letters already used: " + wrongLettersUsed + "</p>"; 
+        
+
+    document.querySelector("#gameDashboard").innerHTML = html;
+
+    initGame();
+  }// end of if you won
+
+  else if(guessesRemaining === 0){
+    alert("You lost!");
+
+    initGame();
+  }// end of if you lost
+
+}//end of cycleComplete()
 
 
 
 
 
 
-//Main Process
 
-initGame();
+//MAIN PROCESSING  ************************************************************************************
+
+document.onkeyup = function(event){
+  gameStarted = true;
+  initGame();
+}
+
+//initGame();
+
 
 document.onkeyup = function(event){
   var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-  alert(letterGuessed);
+  alert("You pressed: " + letterGuessed);
   console.log(letterGuessed);
+  examineLetters(letterGuessed);
+  cycleComplete();
 }
 
 
