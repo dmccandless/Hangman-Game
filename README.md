fe74cdb9c8c06f244cf0fd60b041636155f3af6b
Douglas McCandless <mccandlessd@gmail.com>
2018-03-04 19:04:52 -0500

    add in additional game.js state objects to bring total up to 50 and in images additional flag images to bring total up to 50; in game.js, fixed bug at end of examineLetters() that gave bonus info about state capitals to losers; edited README.md to describe theme and rules of game

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Hangman-Game
 Simple Hangman Game
+This app takes the traditional children's word game and adapts it into a quiz about the state capitals of the US states.
+The player is presented onscreen with a row of blanks representing the letters of a state capital.
+In an aside, the player is presented with an image of the flag of the state for which the city is the capital.
+By touching keys, the player selects letters to fill the blanks.
+Correct letter guesses are filled in the blanks onscreen.
+Incorrect letter guesses are listed onscreen in an "incorrect letter guesses" area.
+A player is given the traditional nine incorrect letter guesses before he/she loses.  (The number of nine relates to the number of strokes used to construct a traditional "hanged" human figure.)
+A player wins by filling in all the onscreen blanks with the correct letters, thereby revealing the state capital's name.
+If a player wins, he/she is rewarded with a short descriptive phrase about the capital.
+When a game ends, win or lose, the player is automatically presented with a new game.
+In any one series of games, the number of the player's wins is kept track of and displayed on the screen.
+The number of the player's losses is NOT kept track of and displayed onscreen.  After all, it's only a game and we're just trying to have fun.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,13 @@
 
 //GLOBAL VARIABLES  ********************************************************************
 
-var stateCapitals = ["montgomery", "tallahassee", "annapolis", "bismarck", "columbia"];/* "augusta",
+var stateCapitals = ["montgomery", "tallahassee", "annapolis", "bismarck", "columbia", "augusta",
   "concord","montpelier", "boston", "providence", "hartford", "albany", "trenton", "harrisburg",
   "dover", "richmond", "charleston", "raleigh", "austin", "batonrouge", "jackson", "lansing", "columbus",
   "frankfort", "nashville", "madison", "springfield", "indianapolis", "stpaul", "desmoines", "jeffersoncity",
   "littlerock", "pierre", "lincoln", "honolulu", "juneau", "olympia", "salem", "sacramento", "boise",
   "carsoncity", "phoenix", "helena", "denver", "topeka", "oklahomacity", "santafe", "saltlakecity",
-  "cheyenne", "atlanta"];*/
+  "cheyenne", "atlanta"];
 
 var computerSelection = "";
 
@@ -63,7 +63,275 @@ var columbia = {
   "nameIs": "Columbia"
 };
 
+var augusta = {
+  "state": "Maine",
+  "flag": "maineFlag",
+  "nameIs": "Augusta"
+};
 
+var concord = {
+  "state": "New Hampshire",
+  "flag": "newHampshireFlag",
+  "nameIs": "Concord"
+};
+
+var montpelier = {
+  "state": "Vermont",
+  "flag": "vermontFlag",
+  "nameIs": "Montpelier"
+};
+
+var boston = {
+  "state": "Massachusetts",
+  "flag": "massachusettsFlag",
+  "nameIs": "Boston"
+};
+
+var providence = {
+  "state": "Rhode Island",
+  "flag": "rhodeIslandFlag",
+  "nameIs": "Providence"
+};
+
+var hartford = {
+  "state": "Connecticut",
+  "flag": "connecticutFlag",
+  "nameIs": "Hartford"
+};
+
+var albany = {
+  "state": "New York",
+  "flag": "newYorkFlag",
+  "nameIs": "Albany"
+};
+
+var trenton = {
+  "state": "New Jersey",
+  "flag": "newJerseyFlag",
+  "nameIs": "Trenton"
+};
+
+var harrisburg = {
+  "state": "Pennsylvania",
+  "flag": "pennsylvaniaFlag",
+  "nameIs": "Harrisburg"
+};
+
+var dover = {
+  "state": "Delaware",
+  "flag": "delawareFlag",
+  "nameIs": "Dover"
+};
+
+var richmond = {
+  "state": "Virginia",
+  "flag": "virginiaFlag",
+  "nameIs": "Richmond"
+};
+
+var charleston = {
+  "state": "West Virginia",
+  "flag": "westVirginiaFlag",
+  "nameIs": "Charleston"
+};
+
+var raleigh = {
+  "state": "North Carolina",
+  "flag": "northCarolinaFlag",
+  "nameIs": "Raleigh"
+};
+
+var austin = {
+  "state": "Texas",
+  "flag": "texasFlag",
+  "nameIs": "Austin"
+};
+
+var batonrouge = {
+  "state": "Louisiana",
+  "flag": "louisianaFlag",
+  "nameIs": "Baton Rouge"
+};
+
+var jackson = {
+  "state": "Mississippi",
+  "flag": "mississippsFlag",
+  "nameIs": "Jackson"
+};
+
+var lansing = {
+  "state": "Michigan",
+  "flag": "michiganFlag",
+  "nameIs": "Lansing"
+};
+
+var frankfort = {
+  "state": "Kentucky",
+  "flag": "kentuckyFlag",
+  "nameIs": "Frankfort"
+};
+
+var nashville = {
+  "state": "Tennessee",
+  "flag": "tennesseeFlag",
+  "nameIs": "nashville"
+};
+
+var columbus = {
+  "state": "Ohio",
+  "flag": "ohioFlag",
+  "nameIs": "Columbus"
+};
+
+var stpaul = {
+  "state": "Minnesota",
+  "flag": "minnesotaFlag",
+  "nameIs": "St. Paul"
+};
+
+var desmoines = {
+  "state": "Iowa",
+  "flag": "iowaFlag",
+  "nameIs": "Des Moines"
+};
+
+var jeffersoncity = {
+  "state": "Missouri",
+  "flag": "missouriFlag",
+  "nameIs": "Jefferson City"
+};
+
+var littlerock = {
+  "state": "Arkansas",
+  "flag": "arkansasFlag",
+  "nameIs": "Little Rock"
+};
+
+var pierre = {
+  "state": "South Dakota",
+  "flag": "southDakotaFlag",
+  "nameIs": "Pierre"
+};
+
+var lincoln = {
+  "state": "Nebraska",
+  "flag": "nebraskaFlag",
+  "nameIs": "Lincoln"
+};
+
+var honolulu = {
+  "state": "Hawaii",
+  "flag": "hawaiiFlag",
+  "nameIs": "Honolulu"
+};
+
+var juneau = {
+  "state": "Alaska",
+  "flag": "alaskaFlag",
+  "nameIs": "Juneau"
+};
+
+var olympia = {
+  "state": "Washington",
+  "flag": "washingtonFlag",
+  "nameIs": "Olympia"
+};
+
+var salem = {
+  "state": "Oregon",
+  "flag": "oregonFlag",
+  "nameIs": "Salem"
+};
+
+var sacramento = {
+  "state": "California",
+  "flag": "californiaFlag",
+  "nameIs": "Sacramento"
+};
+
+var boise = {
+  "state": "Idaho",
+  "flag": "idahoFlag",
+  "nameIs": "Boise"
+};
+
+var carsoncity = {
+  "state": "Nevada",
+  "flag": "nevadaFlag",
+  "nameIs": "Carson City"
+};
+
+var phoenix = {
+  "state": "Arizona",
+  "flag": "arizonaFlag",
+  "nameIs": "Phoenix"
+};
+
+var helena = {
+  "state": "Montana",
+  "flag": "montanaFlag",
+  "nameIs": "Helena"
+};
+
+var denver = {
+  "state": "Colorado",
+  "flag": "coloradoFlag",
+  "nameIs": "Denver"
+};
+
+var topeka = {
+  "state": "Kansas",
+  "flag": "kansasFlag",
+  "nameIs": "Topeka"
+};
+
+var oklahomacity = {
+  "state": "Oklahoma",
+  "flag": "oklahomaFlag",
+  "nameIs": "Oklahoma City"
+};
+
+var santafe = {
+  "state": "New Mexico",
+  "flag": "newMexicoFlag",
+  "nameIs": "Santa Fe"
+};
+
+var saltlakecity = {
+  "state": "Utah",
+  "flag": "utahFlag",
+  "nameIs": "Salt Lake City"
+};
+
+var cheyenne = {
+  "state": "Wyoming",
+  "flag": "wyomingFlag",
+  "nameIs": "Cheyenne"
+};
+
+var atlanta = {
+  "state": "Georgia",
+  "flag": "georgiaFlag",
+  "nameIs": "Atlanta"
+};
+
+var madison = {
+  "state": "Wisconsin",
+  "flag": "wisconsinFlag",
+  "nameIs": "Madison"
+};
+
+var springfield = {
+  "state": "Illinois",
+  "flag": "illinoisFlag",
+  "nameIs": "Springfield"
+};
+
+var indianapolis = {
+  "state": "Indiana",
+  "flag": "indianaFlag",
+  "nameIs": "Indianapolis"
+};
 
 
 
@@ -109,7 +377,7 @@ function initGame(){
 
 }//end of initGame()
 
-//part of attempt to keep duplicates of wrong guesses from being recorded -- that is, record wrong guesses only once
+//keep duplicates of wrong guesses from being recorded -- that is, record wrong guesses only once
 function inArray(needle, haystack){
   var length = haystack.length;
   for (var i = 0; i < length; i++){
@@ -162,12 +430,15 @@ function examineLetters(letter){
   document.querySelector("#gameDashboard").innerHTML = html;
   console.log(html);
 
-  if(lettersInSelection.toString() == currentContest.toString() || guessesRemaining == 0){
-    html1 = "This city is the capital of " + eval(computerSelection).state + " and its name is " + eval(computerSelection).nameIs + ".";
+  if(lettersInSelection.toString() == currentContest.toString()){
+    html1 = "This city is the capital of " + eval(computerSelection).state + 
+    	" and its name is " + eval(computerSelection).nameIs + ".";
     document.querySelector("#cityInfo").innerHTML = html1;
     setTimeout(wrapUpGame, 2000);
-
+  } else if(guessesRemaining == 0){
+		setTimeout(wrapUpGame, 2000);
   }
+
 
 } //end of examine letters
 
